d54956ae8658fd71a8956ca0e833393ac823c32f
bibi03331 <seb_laporte_33@yahoo.com>
2015-05-20 09:30:17 +0200

    Commentaires parametres
    Ajout de commentaire sur les paramètres du programme

--- parametres.py
+++ parametres.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python
 
+# GPIO capteur ultrason avant
 GPIO_TRIGGER = 23
 GPIO_ECHO = 24
 
-DEV_DIRECTION = 'P1-12'
-DEV_VITESSE = 'P1-11'
-
+# Delais entre 2 mesure d'un capteur ultrason
 DELAY_MESURE = 0.05
 
-BIND_IP = 192.168.1.95
-CLIENT_TCP_PORT = 10200
+# Identifiants servoblaster
+DEV_DIRECTION = 'P1-12'     # Servo-moteur de direction
+DEV_VITESSE = 'P1-11'       # Variateur de vitesse
 
+# Adresse IP d'ecoute du serveur TCP
+BIND_IP = 192.168.1.95
+# Port d'ecoute du serveur TCP
+CLIENT_TCP_PORT = 10200
+# Taille du buffer de reception TCP
 CLIENT_TCP_BUFFER_SIZE = 1024
